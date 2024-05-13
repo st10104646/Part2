12fb8feb6c5dbfa17ab2511309a1857c75230fa0
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-05-13 14:56:19 +0000

    Add project files.

---
+++ Part2/Models/details.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Part2.Models
+{
+    public class details : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    
+
+}
